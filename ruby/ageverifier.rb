3c11eceeaa7b48546b533c0ae2cf9fbe6b3886a2
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-10-05 17:30:35 -0500

    Add ability for age verifier to accept array and check ages, in ruby

--- ruby/ageverifier.rb
+++ ruby/ageverifier.rb
@@ -8,3 +8,16 @@ end
 
 verifier(20)
 verifier(21)
+
+def other_verifier(ages)
+  ages.each do |a|
+    if a < 21
+      puts "Too young, you're #{a}"
+    else
+      puts "I see you're #{a}, proceed"
+    end
+  end
+end
+
+ageArray = [15,21,45,19,20,22]
+other_verifier(ageArray )
