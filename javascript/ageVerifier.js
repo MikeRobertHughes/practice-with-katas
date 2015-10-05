1827e2a7604bdfabdad4fbbbdeb3e3353a7932df
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-10-05 17:26:56 -0500

    Add ability for age verifier to accept array and check ages, in javascript

--- javascript/ageVerifier.js
+++ javascript/ageVerifier.js
@@ -8,3 +8,16 @@ function ageVerifier(age) {
 
 ageVerifier(20)
 ageVerifier(21)
+
+function otherVerifier(otherages) {
+  for (var i = otherages.length - 1; i >= 0; i--) {
+    if (otherages[i] < 21) {
+      console.log("Too young, you're " + otherages[i])
+    } else {
+      console.log("I see you're " + otherages[i] + ", proceed")
+    };
+  };
+}
+
+ageArray = [15,21,45,19,20,22]
+otherVerifier(ageArray)
