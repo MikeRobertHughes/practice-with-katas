19eee81051fe7cee92d57a8febfc9c05394f3f49
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-10-05 15:41:17 -0500

    Finish age verifier kata javascript

---
+++ javascript/ageVerifier.js
@@ -0,0 +1,10 @@
+function ageVerifier(age) {
+  if(age < 21){
+    console.log("You're too young to enter")
+  } else {
+    console.log("You may proceed.")
+  }
+}
+
+ageVerifier(20)
+ageVerifier(21)
